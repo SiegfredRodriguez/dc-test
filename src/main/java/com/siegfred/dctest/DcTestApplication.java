752db42c57cd993c6e3fe3f354152f67edf4744a
J-dar Siegfred Rodriguez <siegfred.rodriguez@gmail.com>
2023-09-24 20:58:10 +0800

    dx-test: initial

---
+++ src/main/java/com/siegfred/dctest/DcTestApplication.java
@@ -0,0 +1,13 @@
+package com.siegfred.dctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DcTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/siegfred/dctest/DcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siegfred.dctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
